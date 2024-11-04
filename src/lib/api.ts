b2921def638be3729df19cfdd9ac1d1779d79200
watahaizian <watahaizian@gmail.com>
2024-11-05 04:11:37 +0900

    update api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,9 +11,27 @@ export const fetchPuzzles = async () => {
   return data;
 };
 
+export const postPuzzle = async (puzzle: cellData[]) => {
+  const response = await fetch(
+    'https://nono-api.watahaizian.workers.dev/puzzles',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(puzzle),
+    }
+  );
+  if (!response.ok) {
+    throw new Error('Failed to post puzzle');
+  }
+  const data = await response.json();
+  return data;
+}
+
 export const fetchCells = async (puzzleId: number): Promise<cellData[]> => {
   const response = await fetch(
-    `https://nono-api.watahaizian.workers.dev/cells/${puzzleId}`
+    `https://nono-api.watahaizian.workers.dev/puzzles/${puzzleId}/cells`
   );
   if (!response.ok) {
     throw new Error('Failed to fetch cells');
