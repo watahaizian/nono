5160cd673679ecb91765dfb042b357d9e3d66f4d
watahaizian <watahaizian@gmail.com>
2024-12-14 21:26:42 +0900

    Google認証追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,64 @@
-import React, { useState } from 'react';
-import TitleScreen from './components/TitleScreen';
-import GameScreen from './components/GameScreen';
-import EditScreen from './components/EditScreen';
-import { fetchPuzzles } from './lib/api';
+import type React from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TitleScreen from "./components/TitleScreen";
+import GameScreen from "./components/GameScreen";
+import EditScreen from "./components/EditScreen";
+import AuthCallback from "./components/AuthCallback";
+import { fetchPuzzles } from "./lib/api";
 
 const App: React.FC = () => {
-  const [currentScreen, setCurrentScreen] = useState('title');
-  const [puzzleId, setPuzzleId] = useState(0);
-  const [puzzleSize, setPuzzleSize] = useState(0);
+	const [currentScreen, setCurrentScreen] = useState("title");
+	const [puzzleId, setPuzzleId] = useState(0);
+	const [puzzleSize, setPuzzleSize] = useState(0);
 
-  const startGame = async () => {
-    try {
-      const puzzles = await fetchPuzzles();
-      const randomPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
-      setPuzzleId(randomPuzzle.puzzle_id);
-      setPuzzleSize(randomPuzzle.puzzle_size);
-      setCurrentScreen('game');
-    } catch (error) {
-      console.error(error);
-      alert('Failed to start the game');
-    }
-  };
+	const startGame = async () => {
+		try {
+			const puzzles = await fetchPuzzles();
+			const randomPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
+			setPuzzleId(randomPuzzle.puzzle_id);
+			setPuzzleSize(randomPuzzle.puzzle_size);
+			setCurrentScreen("game");
+		} catch (error) {
+			console.error(error);
+			alert("Failed to start the game");
+		}
+	};
 
-  const startEdit = () => {
-    setCurrentScreen('edit');
-  };
+	const startEdit = () => {
+		setCurrentScreen("edit");
+	};
 
-  // タイトルに戻る
-  const backToTitle = () => {
-    setCurrentScreen('title');
-  };
+	// タイトルに戻る
+	const backToTitle = () => {
+		setCurrentScreen("title");
+	};
 
-  const renderScreen = () => {
-    switch (currentScreen) {
-      case 'title':
-        return <TitleScreen onStart={startGame} onEdit={startEdit} />;
-      case 'game':
-        return <GameScreen puzzleId={puzzleId} puzzleSize={puzzleSize} onBack={backToTitle} />;
-      case 'edit':
-        return <EditScreen onBack={backToTitle} />;
-    }
-  };
+	const renderScreen = () => {
+		switch (currentScreen) {
+			case "title":
+				return <TitleScreen onStart={startGame} onEdit={startEdit} />;
+			case "game":
+				return (
+					<GameScreen
+						puzzleId={puzzleId}
+						puzzleSize={puzzleSize}
+						onBack={backToTitle}
+					/>
+				);
+			case "edit":
+				return <EditScreen onBack={backToTitle} />;
+		}
+	};
 
-  return <div>{renderScreen()}</div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={renderScreen()} />
+				<Route path="/auth/callback" element={<AuthCallback />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
